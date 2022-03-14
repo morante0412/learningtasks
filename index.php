f9c2bef83c61689b01c50e7c4803f54162d5e699
BlisterndaSun <100022310+morante0412@users.noreply.github.com>
2022-03-14 14:04:56 +0800

    Update index.php

--- index.php
+++ index.php
@@ -7,7 +7,8 @@
         <h1> Welcome To My Website! I am Mark Joshua P. Morante!</h1>
         <ul>
             <li><a href="index.php">HOME</a></li>
-            <li><a href="/LT2/index.html">LEARNING TASK 2</a></li>
+            <li><a href="/LT2/menu.html">LEARNING TASK 2</a></li>
+            <li><a href="/LT3/index.html">LEARNING TASK 2</a></li>
         </ul>
     </body>
 </html>
